f2df075210baf64ae11dfb8ca64317b25c17a859
funbiscuit <kokurinsv@gmail.com>
2021-03-30 22:54:40 +0300

    Add tests for multiple commands in a row

--- tests/EmbeddedCliTest.cpp
+++ tests/EmbeddedCliTest.cpp
@@ -43,5 +43,33 @@ TEST_CASE("EmbeddedCli", "[cli]") {
         REQUIRE(commands.back().args == "led 1 1");
     }
 
+    SECTION("Test sending multiple commands") {
+        for (int i = 0; i < 3; ++i) {
+            mock.sendLine("set led 1 " + std::to_string(i));
+        }
+        embeddedCliProcess(cli);
+
+        REQUIRE(commands.size() == 3);
+        for (int i = 0; i < 3; ++i) {
+            REQUIRE(commands[i].name == "set");
+            REQUIRE(commands[i].args == ("led 1 " + std::to_string(i)));
+        }
+    }
+
+    SECTION("Test buffer overflow recovery") {
+        for (int i = 0; i < 100; ++i) {
+            mock.sendLine("set led 1 " + std::to_string(i));
+        }
+        embeddedCliProcess(cli);
+        REQUIRE(commands.size() < 100);
+        commands.clear();
+
+        mock.sendLine("set led 1 150");
+        embeddedCliProcess(cli);
+        REQUIRE(commands.size() == 1);
+        REQUIRE(commands.back().name == "set");
+        REQUIRE(commands.back().args == "led 1 150");
+    }
+
 }
 
