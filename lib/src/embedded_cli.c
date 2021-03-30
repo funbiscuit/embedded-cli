394b6aadf7139445c2df2d9333f6ddaee95aab6f
funbiscuit <kokurinsv@gmail.com>
2021-03-30 20:59:08 +0300

    Add creation of cli

--- lib/src/embedded_cli.c
+++ lib/src/embedded_cli.c
@@ -6,14 +6,35 @@
 #define PREPARE_IMPL(t) \
   EmbeddedCliImpl* impl = (EmbeddedCliImpl*)t->_impl;
 
+#define RX_BUFFER_SIZE 64
+
 typedef struct EmbeddedCliImpl EmbeddedCliImpl;
 
 struct EmbeddedCliImpl {
-    uint8_t i;
+    char rxBuffer[RX_BUFFER_SIZE];
+
+    /**
+     * Current size of buffer
+     */
+    uint8_t rxSize;
 };
 
+
 EmbeddedCli *embeddedCliNew(void) {
-    return NULL;
+    EmbeddedCli *cli = malloc(sizeof(EmbeddedCli));
+
+    if (cli == NULL)
+        return NULL;
+    memset(cli, 0, sizeof(EmbeddedCli));
+
+    cli->_impl = malloc(sizeof(EmbeddedCliImpl));
+    if(cli->_impl == NULL) {
+        free(cli);
+        return NULL;
+    }
+    memset(cli->_impl, 0, sizeof(EmbeddedCliImpl));
+
+    return cli;
 }
 
 void embeddedCliReceiveChar(EmbeddedCli *cli, char c) {
@@ -25,5 +46,6 @@ void embeddedCliProcess(EmbeddedCli *cli) {
 }
 
 void embeddedCliFree(EmbeddedCli *cli) {
-
+    free(cli->_impl);
+    free(cli);
 }
