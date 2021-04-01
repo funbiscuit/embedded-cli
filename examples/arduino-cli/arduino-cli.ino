347d525df86b626f2cbf53377f80b2f2fec1b19f
funbiscuit <kokurinsv@gmail.com>
2021-04-01 23:06:40 +0300

    Update arduino example to use static allocation

--- examples/arduino-cli/arduino-cli.ino
+++ examples/arduino-cli/arduino-cli.ino
@@ -1,25 +1,49 @@
 /**
  * Simple example of using embedded-cli in arduino.
  * To compile copy embedded-cli.h and embedded-cli.c to sketch directory.
+ *
+ * With specified settings (32 bytes for RX buffer and no dynamic allocation)
+ * library uses 1024 bytes of ROM and 85 bytes of RAM.
+ * Total size of firmware is 2576 bytes, 269 bytes of RAM are used.
  */
 
 #include "embedded_cli.h"
 
+// cli uses 15 extra bytes for internal structures (on Arduino Nano)
+#define CLI_BUFFER_SIZE 47
+#define CLI_RX_BUFFER_SIZE 32
+
 EmbeddedCli *cli;
+
+uint8_t cliBuffer[CLI_BUFFER_SIZE];
 
 void onCommand(EmbeddedCli *embeddedCli, CliCommand *command);
 
 void writeChar(EmbeddedCli *embeddedCli, char c);
 
 void setup() {
-    cli = embeddedCliNew();
+    Serial.begin(9600);
+
+    EmbeddedCliConfig *config = embeddedCliDefaultConfig();
+    config->cliBuffer = cliBuffer;
+    config->cliBufferSize = CLI_BUFFER_SIZE;
+    config->rxBufferSize = CLI_RX_BUFFER_SIZE;
+    cli = embeddedCliNew(config);
+
+    if (cli == NULL) {
+        Serial.println(F("Cli was not created. Check sizes!"));
+        return;
+    }
+    Serial.println(F("Cli has started. Enter your commands."));
+
     cli->onCommand = onCommand;
     cli->writeChar = writeChar;
-
-    Serial.begin(9600);
 }
 
 void loop() {
+    if (cli == NULL)
+        return;
+
     // provide all chars to cli
     while (Serial.available() > 0) {
         embeddedCliReceiveChar(cli, Serial.read());
