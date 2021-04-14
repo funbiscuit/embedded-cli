39edb0a3aea4a9f3e9c7f955ed66b872e328a33c
funbiscuit <kokurinsv@gmail.com>
2021-04-14 18:21:00 +0300

    Refactor tests structure
    Add separate runner and use separate functions to group tests

---
+++ tests/CliTestRunner.h
@@ -0,0 +1,27 @@
+#ifndef EMBEDDED_CLI_CLITESTS_H
+#define EMBEDDED_CLI_CLITESTS_H
+
+#include "embedded_cli.h"
+#include "CliMock.h"
+
+class CliTestRunner {
+public:
+
+    explicit CliTestRunner(EmbeddedCli *cli);
+
+    void runTests();
+
+private:
+    EmbeddedCli *cli;
+    CliMock mock;
+
+    void testBase();
+
+    void testAutocomplete();
+
+    void testPrinting();
+
+    void testHelp();
+};
+
+#endif //EMBEDDED_CLI_CLITESTS_H
